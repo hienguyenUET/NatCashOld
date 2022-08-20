b2105533741fd05c4eec44deafd467d6b664918f
Hien Nguyen UET <hien43669@gmail.com>
2022-08-20 17:36:51 +0700

    update home screen

--- java/com/example/natcash/HomeScreen.kt
+++ java/com/example/natcash/HomeScreen.kt
@@ -1,23 +1,34 @@
 package com.example.natcash
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class HomeScreen : AppCompatActivity() {
     private lateinit var textView:TextView;
     private lateinit var sharedPreferences: SharedPreferences;
 
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
-        sharedPreferences = getSharedPreferences("com.example.natcash", MODE_PRIVATE)
+       sharedPreferences = getSharedPreferences("com.example.natcash", MODE_PRIVATE)
         val phoneNumber:String? = sharedPreferences.getString("phoneNumber","Empty")
         println(phoneNumber)
+        if (sharedPreferences.contains("phoneNumber")) println("yes")
         textView = findViewById(R.id.phoneNumber)
         textView.text = phoneNumber
+    }
+
+    fun logout(view: View) {
+        sharedPreferences = getSharedPreferences("com.example.natcash", MODE_PRIVATE)
+        sharedPreferences.edit().remove("phoneNumber").apply();
+        val intent = Intent(this, LoginScreen::class.java)
+        startActivity(intent)
     }
 }
