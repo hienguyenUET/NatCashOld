9298ababde53b03f7f707e0ea8704f09c7100993
Hien Nguyen UET <hien43669@gmail.com>
2022-08-22 13:44:51 +0700

    add more icon to home screen

---
+++ java/com/example/natcash/ButtonInfo.kt
@@ -0,0 +1,7 @@
+package com.example.natcash
+
+import android.graphics.drawable.GradientDrawable
+import android.widget.ImageButton
+import android.widget.TextView
+
+data class ButtonInfo(val imageResource:Int, val textDescription:String?, val background:Int)
--- java/com/example/natcash/HomeScreen.kt
+++ java/com/example/natcash/HomeScreen.kt
@@ -3,22 +3,39 @@ package com.example.natcash
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeScreen : AppCompatActivity() {
-    private lateinit var textView:TextView;
+    private lateinit var textView: TextView;
     private lateinit var sharedPreferences: SharedPreferences;
+//    private var gridLayout: GridLayoutManager = R.id.homeButtonRecyclerView
+    private var adapter: ButtonListAdapter? = null
+    private var recyclerView: RecyclerView? = null
+    private var btnList = ArrayList<ButtonInfo>()
 
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
-       sharedPreferences = getSharedPreferences("com.example.natcash", MODE_PRIVATE)
-        val phoneNumber:String? = sharedPreferences.getString("phoneNumber","Empty")
+        getPhoneNumber()
+
+        //define recyclerview
+//        recyclerView = findViewById(R.id.homeButtonRecyclerView)
+//        gridLayout = GridLayoutManager(this, 2)
+//        recyclerView.apply {gridLayout = GridLayoutManager(applicationContext, 3) }
+//        getButton()
+
+    }
+
+    private fun getPhoneNumber() {
+        sharedPreferences = getSharedPreferences("com.example.natcash", MODE_PRIVATE)
+        val phoneNumber: String? = sharedPreferences.getString("phoneNumber", "Empty")
         println(phoneNumber)
         if (sharedPreferences.contains("phoneNumber")) println("yes")
         textView = findViewById(R.id.phoneNumber)
@@ -31,4 +48,5 @@ class HomeScreen : AppCompatActivity() {
         val intent = Intent(this, LoginScreen::class.java)
         startActivity(intent)
     }
+
 }
